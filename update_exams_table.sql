9b14b32da172d085662a55d4f0084f5244d1ef51
“Nguyen_Duc_Canh” <nduccanh2003@gmail.com>
2025-03-09 14:02:30 +0700

    Xóa file sql cũ

--- update_exams_table.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `exams`
-ADD COLUMN `title` varchar(255) NOT NULL,
-ADD COLUMN `description` text NOT NULL,
-ADD COLUMN `duration` int(11) NOT NULL,
-ADD COLUMN `total_marks` int(11) NOT NULL,
-ADD COLUMN `subject` varchar(255) NOT NULL;
